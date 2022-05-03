ef1a381ccbed972b4d4e7bf9c1ebffbbb807e368
unknown <jerrytan7@gmail.com>
2022-05-03 14:59:46 -0400

    search page + some ui for the nav bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
 import React, { useState, useContext } from 'react';
 import OnboardingPopup from './OnboardingPopup';
 import Modal from 'react-modal';
-import LogoutButton from './LogoutButton';
-import { AccountContext } from '../contexts/AccountContext'
+import { AccountContext } from '../contexts/AccountContext';
+import Item from './Item';
+import { GET_ITEM_PAGINATION } from '../graphql/queries/getItemPagination';
+import { useLazyQuery, useQuery } from "@apollo/client";
+
 const customStyles = {
     content: {
       top: '50%',
@@ -18,15 +21,61 @@ const customStyles = {
 export default function Home() {
     const { user } = useContext(AccountContext);
     const [displayOnboard, setDisplayOnboard] = useState(!user.is_onboarded);
+    const [cursor, setCursor] = useState("");
+    const [items, setItems] = useState([]);
+
+    useQuery(GET_ITEM_PAGINATION, {
+        variables: {
+            itemPaginationInput: {
+                quantity: 20,
+                cursor_type: "updated_at"
+            }
+        },
+        onCompleted: (data) => {
+            const { getItemPagination: { items, cursor }} = data;
+            setCursor(cursor);
+            setItems(items);
+        }
+    });
+
+    const [loadMoreItems] = useLazyQuery(GET_ITEM_PAGINATION, {
+        variables: {
+            itemPaginationInput: {
+                quantity: 20,
+                cursor,
+                cursor_type: "updated_at"
+            }
+        },
+    });
+
+    const handleShowMore = async () => {
+        const result = await loadMoreItems();
+        const { data: { getItemPagination: { items: newItems, cursor } } } = result;
+        setItems([...items, ...newItems]);
+        setCursor(cursor);
+    }
 
     return (
         <div>
-            <LogoutButton/>
             <h3>Welcome {user.first_name}</h3>
             <h1>Home page: where you can search for items (future recommended page)</h1>
             <Modal isOpen={displayOnboard} style={customStyles} ariaHideApp={false}>
                 <OnboardingPopup setDisplayOnboard={setDisplayOnboard}/>
             </Modal>
+            <div className="grid grid-cols-5 gap-4 mx-16 my-8"> 
+                <div className=""> Search Query Options (In Progress) </div>
+                <div className="col-span-4 grid grid-cols-5 gap-4"> 
+                    {items.map((item, idx) => <Item item={item} key={idx}></Item>)} 
+                </div>
+                <div></div>
+                <div className="col-span-4 grid place-items-center">
+                    {  
+                        cursor 
+                            ? <button onClick={handleShowMore} className="bg-blue-100 p-1 rounded-md">Show More</button>
+                            : ""
+                    }
+                </div>
+            </div>
         </div>
         
     )
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import Modal from 'react-modal';
 import ItemInputPopup from './ItemInputPopup';
+import { Link } from 'react-router-dom';
+import LogoutButton from './LogoutButton';
 
 export default function Nav() {
 
@@ -20,29 +21,30 @@ export default function Nav() {
   const [displayItemInput, setDisplayItemInput] = useState(false);
   return (
     <div>
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">Fair Trade</Link>
-        <button onClick={() => { setDisplayItemInput(true) }}>Create New Item</button>
-        <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/home" className="nav-link">Home</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/login" className="nav-link">Login</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/createAccount" className="nav-link">Create Account</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/profile" className="nav-link">Profile</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="trades" className="nav-link">Trades</Link>
-          </li>      
-        </ul>
+      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+          <a href="/home" className="flex items-center">
+              <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/81/pregnant-woman_1f930.png" className="mr-3 h-6 sm:h-9" alt="FairTrade Logo" />
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">FairTrade</span>
+          </a>
+          <button onClick={() => { setDisplayItemInput(true) }} className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700\">Create New Item</button>
+          <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
+            <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+              <li>
+                <Link to="/home" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</Link>
+              </li>
+              <li>
+                <Link to="/trades" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Trades</Link>
+              </li>
+              <li>
+                <Link to="/profile" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Profile</Link>
+              </li>
+            </ul>
+          </div>
+          <LogoutButton/>
         </div>
       </nav>
+      
       <Modal isOpen={displayItemInput} style={customStyles} ariaHideApp={false}>
           <ItemInputPopup onClose={() => {setDisplayItemInput(false)}}/>
       </Modal>
