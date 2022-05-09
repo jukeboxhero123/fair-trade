76744e96477799867a0027d7c444afad6f312949
jukeboxhero123 <jerrytan7@gmail.com>
2022-05-09 12:38:04 -0400

    some css

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -1,6 +1,16 @@
 import React from 'react';
 export default function Trades() {
+
+    
+
     return (
-        <h1>Trades page: displays all pending trades, this is where you chat with people</h1>
+        <div className="m-6 h-5/6 bg-green-200">
+            <div className="w-52 h-full bg-lime-200 float-left"> 
+                Trades Here
+            </div>
+            <div className="w-full h-full bg-red-200"> 
+                Chat Here
+            </div>
+        </div>
     )
 }
